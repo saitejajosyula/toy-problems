1ebc7542301549271d7f10eab534e731cc76c847
saitejajosyula <saitejajosyula@msitprogram.net>
2020-04-13 20:03:56 +0530

    added testcases

---
+++ lru.py
@@ -0,0 +1,17 @@
+class lru:
+
+    def __init__(self, size, data):
+        self.size = size
+        self.data = data
+
+    def get(self, key, default=None):
+
+        pass
+
+    def put(self, key):
+
+        pass
+
+    def get_cache(self):
+
+        pass

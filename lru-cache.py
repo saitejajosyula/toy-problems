246bf862af1cb1ea3e73363c0fa75f42283cba09
saitejajosyula <saitejajosyula@msitprogram.net>
2020-04-13 16:57:27 +0530

    created public methods

--- lru-cache.py
+++ lru-cache.py
@@ -2,3 +2,16 @@ class lru:
   def __init__(self, size, value):
     self.size = size
     self.value = value
+
+    def put(self, key, value):
+
+        pass
+
+    def get(self, key, default=None):
+
+        pass
+
+    def get_cache(self):
+
+        pass
+
